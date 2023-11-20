0c6429bc335e820dc0c5b9ad6715ce37e2d87991
iury <iury@pop-os.localdomain>
2023-11-20 14:19:44 -0300

    Project base upload

---
+++ src/test/java/com/containermgmt/ContainerManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.containermgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContainerManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
